0faf529b547729976abd945003cd4a3de4b19d5d
張維傑 <jie255057@gmail.com>
2020-01-29 23:37:10 +0800

    rspec edit update

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -5,23 +5,33 @@ RSpec.feature "TaskTest", type: :feature do
     visit root_path
     click_on "新增任務"
     fill_in "標題", with: "Task1"
-    fill_in "內容", with: "content1"
+    fill_in "內容", with: "Content1"
     click_on "送出"
 
     expect(page).to have_content "Task1"
     last_task = Task.last
     expect(last_task.title).to eq("Task1")
-    expect(last_task.content).to eq("content1")
+    expect(last_task.content).to eq("Content1")
+    expect(Task.count).to eq(1)
+  end
+  
+  scenario "編輯任務" do
+    task = Task.create(title: "Task1", content: "Content1")
+    visit root_path
+    click_on "編輯"
+    fill_in "標題", with: "Edit2"
+    fill_in "內容", with: "EditContent2"
+    click_on "送出"
+
+    expect(page).to have_content "Edit2"
+    last_task = Task.last
   end
 
-  # scenario "編輯任務" do
-  #   last_task = Task.last
+  # scenario "刪除資料" do
+  #   task = Task.create(title: "Task1", content: "Content1")
   #   visit root_path
-  #   click_on last_task("編輯")
-  #   fill_in "標題", with: "Edit1"
-  #   fill_in "內容", with: "EditContent1"
-  #   click_on "送出"
-
-  #   expect(page).to have_content "Edit1"
+  #   click_on "刪除"
+    
+  #   expect(Taks.count).to 
   # end
 end
